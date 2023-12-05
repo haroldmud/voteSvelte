7e3c0af6fcef90b0d6f6040afc0957e60f7ff0a8
haroldmud <haroldmudosa@email.com>
2023-12-05 12:36:27 +0200

    feat: create a small counter project for testing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,33 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+  export let name = 0;
+  function handleInc() {
+    return name++;
+  }
+  function handleDec() {
+    return name--;
+  }
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+  <div class="flex justify-center gap-4">
+    <button
+      on:click={handleDec}
+      class="h-fit my-auto px-2 rounded-sm border-2 border-black border-1 bg-blue-300"
+      >dec</button
+    >
+    <h1 class="text-3xl font-bold underline">
+      {name < 10 && name >= 0 ? "0" : ""}{name}
+    </h1>
+    <button
+      on:click={handleInc}
+      class="h-fit my-auto px-2 rounded-sm border-2 border-black border-1 bg-blue-300"
+      >inc</button
+    >
+  </div>
 </section>
 
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<!-- <style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style> -->
