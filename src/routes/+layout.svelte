aef1e474fb468bb504a73d50fbe1c43dd7255579
haroldmud <haroldmudosa@email.com>
2023-12-06 17:36:46 +0200

    feat: practicing the state management using store

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,24 @@
 <script>
   import Header from "./Header.svelte";
+  export let message = '';
+  // import { getContext } from "svelte";
   import "./styles.css";
   import "../app.css";
+  // const  message = getContext('thisKey')
 </script>
 
 <div class="flex flex-col justify-between border h-[100vh] min-w-[100vh]">
-  <Header />
-
-  <main class="flex flex-1 flex-col p-[1rem] w-full max-w-[64rem] mx-auto borde-box">
-    <slot />
-  </main>
+  <div>
+    <Header />
+  
+    <main class="flex flex-1 flex-col p-[1rem] w-full max-w-[64rem] mx-auto borde-box">
+      <slot />
+    </main>
+  </div>
 
   <footer class="flex flex-col justify-center items-center p-[3rem] font-thin w-fit mx-auto border-black border-t">
     <p class="px-[3rem]">
-      copyright 2023
+      copyright 2023 {message}
     </p>
   </footer>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,10 @@
 <script>
+  import { toast } from "./notification/Notification";
+  import { count } from "./notification/Notification";
+  import { inc } from "./notification/Notification";
+  import { dec } from "./notification/Notification";
+  import Toast from "./notification/Toast.svelte";
+
   export let name = 0;
   function handleInc() {
     return name++;
@@ -9,8 +15,21 @@
 </script>
 
 <section>
+  <Toast />
+  <button on:click={inc}>
+    increse
+  </button>
   <div>
-    
+    {count}
+  </div>
+  <button on:click={dec}>
+    decrease
+  </button>
+  <div>
+    <button
+      class="cursor-pointer mt-[30rem]"
+      on:click={()=> toast('️‍🔥 Svelete is fire for real')}
+    >show notification</button>
   </div>
 </section>
 
