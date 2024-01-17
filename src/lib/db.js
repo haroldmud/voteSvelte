68bc082e8da869cf84d37d5a91c11986333c5822
haroldmud <haroldmudosa@email.com>
2024-01-17 20:37:26 +0200

    feat: remove the next public instance

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.NEXT_PUBLIC_VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.NEXT_PUBLIC_VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient('https://teurhfjnskwrxugnxpuu.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRldXJoZmpuc2t3cnh1Z254cHV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUzMTM5ODIsImV4cCI6MjAyMDg4OTk4Mn0.RfQcIUmIw0RfuH3d5WbUkxY9WHRp2PSfLWTHNpgmjBY')
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
